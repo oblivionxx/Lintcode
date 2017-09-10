b1b77f8ed00dc86f1b105382b42db562f25cd480
oblivionxx <oblivion.meteor@gmail.com>
2017-09-10 21:10:32 +0100

    Add new

---
+++ src/Lint597_Subtree_with_Maximum_Average.java
@@ -0,0 +1,16 @@
+/*
+ * Given a binary tree, find the subtree with maximum average. Return the root of the subtree.
+
+Example 
+Given a binary tree:
+
+     1
+   /   \
+ -5     11
+ / \   /  \
+1   2 4    -2 
+return the node 11.
+ */
+public class Lint597_Subtree_with_Maximum_Average {
+    // http://blog.leanote.com/post/westcode/53751fcb4502
+}
